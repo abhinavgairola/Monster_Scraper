e3d59336f24643101c021161b0eded09c6484e09
abhinavgairola <abhinavgairola@Abhinavs-MacBook-Pro.local>
2021-03-29 15:53:43 -0400

    newcodes

--- Plotting.py
+++ Plotting.py
@@ -14,12 +14,14 @@ class plotter:
         data_ = [data[d] for d in (keys) if d.find('content')!=-1]
         All_list = (list(itertools.chain.from_iterable(data_)))
         Frequency = dict(Counter(All_list))
+        sorted_tuples = sorted(Frequency.items(), key = lambda item:item[1])
+        Frequency = {k:v for k, v in sorted_tuples}
         x = list(Frequency.keys())
         y = list(Frequency.values())
         fig_1 = px.bar( x=y, y=x, orientation='h')
         fig_1.update_layout( xaxis={'categoryorder':'category descending'})
         fig_1.write_image('bar'+file_.split('.')[0]+'.png',scale=2)
-        fig_1
+        fig_1.show()
         fig_2 = px.pie(values=y, names=x, title='What skills are needed in data science'+'  '+file_.split('.')[0])
         fig_2.write_image('pie'+file_.split('.')[0]+'.png',scale=2)
         fig_2.show()
